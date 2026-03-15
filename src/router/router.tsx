b4d00180553d3530a46a9fca37d03279c17e46f5
jeancs21 <jeanc.2196@gmail.com>
2026-03-14 22:36:44 -0400

    Created layout

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,10 +2,12 @@ import { createBrowserRouter } from "react-router-dom";
 import UserListPage from "../pages/UserListPage";
 import CreateUserPage from "../pages/CreateUserPage";
 import EditUserPage from "../pages/EditUserPage";
+import MainLayout from "../components/common/layout/MainLayout";
 
 export const router = createBrowserRouter([
     {
         path:"/",
+        element: <MainLayout />,
         children: [
             {
                 path: "",
