492c9dd70ecd9b342759be42c9e81fe93f7ec621
jeancs21 <jeanc.2196@gmail.com>
2026-03-16 13:50:35 -0400

    Added disabled property to Button component.

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -5,11 +5,17 @@ interface ButtonProps {
     className?: string;
     type: 'button' | 'submit' | 'reset';
     onClick?: () => void;
+    disabled?: boolean;
 }
 
-const Button = ({ children, className, type, onClick }: ButtonProps) => {
+const Button = ({ children, className, type, onClick, disabled }: ButtonProps) => {
   return (
-    <button type={type} className={`flex justify-center items-center cursor-pointer ${className}`} onClick={onClick}>
+    <button 
+      type={type} 
+      className={`flex justify-center items-center transition-all ${disabled ? 'opacity-50 cursor-not-allowed grayscale-[0.5]' : 'cursor-pointer'} ${className}`} 
+      onClick={disabled ? undefined : onClick}
+      disabled={disabled}
+    >
         {children}
     </button>
   )
